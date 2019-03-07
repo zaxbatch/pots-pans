cfa41b5bbef2e96b0e303e7b6bb6b71aebe95c0a
Dotcom <sneakjunkmail@gmail.com>
2019-03-06 20:30:57 -0500

    added images

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
+    //this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
